2d4cc585a67c8cf2236d0a027654b1286b77c61b
Kimberly <kira0001@stud.kea.dk>
2023-12-12 08:47:17 +0100

    index.js removed

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// Hent DOM-elementer
-const menuIcon = document.getElementById("menu-icon");
-const navbar = document.querySelector(".navbar");
-
-// Tilføj klikhåndterer til menu-ikonet
-menuIcon.addEventListener("click", () => {
-  // Skift visning af navbar ved at tilføje/fjerne CSS-klasse
-  navbar.classList.toggle("show-navbar");
-});
